23bc40d3bcd9e5d1d28b3dae5e8d337838ca0550
Luka Kiszko <vimisomagic@gmail.com>
2019-10-18 11:22:49 +0100

    Comment typo amend.

--- example.php
+++ example.php
@@ -5,7 +5,7 @@ use Coinpush\Client;
 
 $client = new Client(new Config);
 $response = $client->create('btc', [
-    'amount' => 200000, // Satoshis to charge (0.0002 * 10000000).
+    'amount' => 200000, // Satoshis to charge (0.0002 * 100000000).
     'output_address' => '142ZaKhcv68Yepqqu5TuQ88kLbBVxcVeRW',
 ]);
 $results = $response['results'];
